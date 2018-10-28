4da86808425ce36340c6602f9dc77cf40ae1ca27
Eva Deželak <eva.dezelak@gmail.com>
2018-10-28 20:14:55 +0100

    osnovni podatki

--- Analiza_studentske_prehrane/analiza_studentske_prehrane.py
+++ Analiza_studentske_prehrane/analiza_studentske_prehrane.py
@@ -13,7 +13,13 @@ imenik = 'imenik_restavracij'
 # ime datoteke v katero bomo shranili glavno stran
 frontpage = 'frontpage.html'
 # ime CSV datoteke v katero bomo shranili podatke
-csv_datoteka = 'imenik_restavracij.csv'
+csv_datoteka = 'vse-restavracije.csv'
+
+
+def pripravi_imenik(ime_datoteke):
+    imenik = os.path.dirname(ime_datoteke)
+    if imenik:
+        os.makedirs(imenik, exist_ok=True)
 
 
 def url_v_niz(url):
@@ -25,19 +31,61 @@ def url_v_niz(url):
     return r.text
 
 
-def shrani_niz(text, directory, filename):
-    os.makedirs(directory, exist_ok=True)
-    path = os.path.join(directory, filename)
+def shrani_niz(text, ime_datoteke, filename):
+    os.makedirs(ime_datoteke, exist_ok=True)
+    path = os.path.join(ime_datoteke, filename)
     with open(path, 'w', encoding='utf-8') as file_out:
         file_out.write(text)
     return None
 
 
-def shrani_frontpage(url, ime_datoteke):
-    vsebina = url_v_niz(url)
-    with open(ime_datoteke, 'w', encoding='utf-8') as datoteka:
-        datoteka.write(vsebina)
-    print('shranjeno!')
+def vsebina_datoteke(ime_datoteke):
+    with open(ime_datoteke, encoding='utf-8') as datoteka:
+        return datoteka.read()
 
 
-shrani_frontpage(url, frontpage)
+def zapisi_csv(slovarji, imena_polj, ime_datoteke):
+    pripravi_imenik(ime_datoteke)
+    with open(ime_datoteke, 'w', encoding='utf-8') as csv_datoteka:
+        writer = csv.DictWriter(csv_datoteka, fieldnames=imena_polj)
+        writer.writeheader()
+        for slovar in slovarji:
+            writer.writerow(slovar)
+
+
+def zapisi_json(objekt, ime_datoteke):
+    pripravi_imenik(ime_datoteke)
+    with open(ime_datoteke, 'w', encoding='utf-8') as json_datoteka:
+        json.dump(objekt, json_datoteka, indent=4, ensure_ascii=False)
+
+
+
+vzorec = re.compile(
+    r'<div class="row restaurant-row (?P<ugodnosti>.*?)".*?'
+    #r'.*? data-doplacilo="(?P<doplacilo>.*?)".*?'
+    r'data-lokal="(?P<ime>.*?)".*?'
+    r'data-city="(?P<kraj>\w*?)"',
+    #r'<small><i>(?P<naslov>.*?)</i>.*?',
+    re.DOTALL
+)
+    
+
+def izloci_podatke(ujemanje):
+    podatki_restavracije = ujemanje.groupdict()
+    podatki_restavracije['ugodnosti'] = podatki_restavracije['ugodnosti'].strip()
+    #podatki_restavracije['doplacilo'] = (podatki_restavracije['doplacilo']).replace(',', '.')
+    #podatki_restavracije['doplacilo'] = float(podatki_restavracije['doplacilo'])
+    podatki_restavracije['ime'] = podatki_restavracije['ime'].strip()
+    #podatki_restavracije['naslov'] = podatki_restavracije['naslov'].strip()
+    podatki_restavracije['kraj'] = podatki_restavracije['kraj'].strip()
+    return podatki_restavracije
+
+podatki_restavracije = []
+stevec = 0
+vsebina = vsebina_datoteke('imenik_restavracij/frontpage.html')
+for ujemanje in vzorec.finditer(vsebina):
+    stevec += 1
+    print(stevec)
+    podatki_restavracije.append(izloci_podatke(ujemanje))
+zapisi_json(podatki_restavracije, 'obdelani-podatki/vse-restavracije.json')
+zapisi_csv(podatki_restavracije, ["ime", "kraj", "ugodnosti"], 'obdelani-podatki/vse-restavracije.csv')
