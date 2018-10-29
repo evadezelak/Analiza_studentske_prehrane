dae6e0d05f8529794a43fa30d1d2ba27b920b32e
Eva Deželak <eva.dezelak@gmail.com>
2018-10-29 18:24:47 +0100

    popravljene vrednosti ugodnosti

--- Analiza_studentske_prehrane/analiza_studentske_prehrane.py
+++ Analiza_studentske_prehrane/analiza_studentske_prehrane.py
@@ -62,8 +62,9 @@ def zapisi_json(objekt, ime_datoteke):
 
 vzorec = re.compile(
     r'<div class="row restaurant-row (?P<vegetarijansko>service-1)? ?(?P<dostop_za_invalide>service-3)? ?(?P<dostava>service-5)? ?(?P<odprto_ob_vikendih>service-20)? ?(?P<nov_lokal>service-69)?.*?"(?:\s|\n)*data.*?'
+    #r'<div class="row restaurant-row .*?(?P<nov_lokal>service-69)?"(?:\s|\n)*data.*?'
     r'.*? data-doplacilo="(?P<doplacilo>.*?)".*?'
-    r'<input checked="checked".*?value="(?P<ocena>\d+)".*?></label>.*?' #Kaj pa če ocene ni?
+    #r'<input checked="checked".*?value="(?P<ocena>\d+)".*?></label>.*?' #Kaj pa če ocene ni?
     r'data-lokal="(?P<ime>.*?)".*?'
     r'data-city="(?P<kraj>.*?)"',
     #r'<small><i>(?P<naslov>.*?)</i>.*?',
@@ -73,10 +74,36 @@ vzorec = re.compile(
 
 def izloci_podatke(ujemanje):
     podatki_restavracije = ujemanje.groupdict()
+    if podatki_restavracije['vegetarijansko'] is not None:
+        podatki_restavracije['vegetarijansko'] = 'DA'
+    else:
+        podatki_restavracije['vegetarijansko'] = 'NE'
+
+    if podatki_restavracije['dostop_za_invalide'] is not None:
+        podatki_restavracije['dostop_za_invalide'] = 'DA'
+    else:
+        podatki_restavracije['dostop_za_invalide'] = 'NE'
+
+    if podatki_restavracije['dostava'] is not None:
+        podatki_restavracije['dostava'] = 'DA'
+    else:
+        podatki_restavracije['dostava'] = 'NE'
+
+    if podatki_restavracije['odprto_ob_vikendih'] is not None:
+        podatki_restavracije['odprto_ob_vikendih'] = 'DA'
+    else:
+        podatki_restavracije['odprto_ob_vikendih'] = 'NE'
+
+    if podatki_restavracije['nov_lokal'] is not None:
+        podatki_restavracije['nov_lokal'] = 'DA'
+    else:
+        podatki_restavracije['nov_lokal'] = 'NE'
+        
     #podatki_restavracije['vegetarijansko'] = podatki_restavracije['vegetarijansko'].replace('Null', 'NE')
     podatki_restavracije['doplacilo'] = (podatki_restavracije['doplacilo']).replace(',', '.')
-    podatki_restavracije['doplacilo'] = float(podatki_restavracije['doplacilo'])
-    podatki_restavracije['ocena'] = int(podatki_restavracije['ocena'])
+    #podatki_restavracije['doplacilo'] = float(podatki_restavracije['doplacilo'])
+    podatki_restavracije['doplacilo'] = (podatki_restavracije['doplacilo']) + '€'
+    #podatki_restavracije['ocena'] = int(podatki_restavracije['ocena'])
     podatki_restavracije['ime'] = podatki_restavracije['ime'].strip()
     podatki_restavracije['ime'] = podatki_restavracije['ime'].replace('&quot;', '"')
     #podatki_restavracije['naslov'] = podatki_restavracije['naslov'].strip()
@@ -89,6 +116,6 @@ for ujemanje in vzorec.finditer(vsebina):
     print('Računam')
     podatki_restavracije.append(izloci_podatke(ujemanje))
 zapisi_json(podatki_restavracije, 'obdelani-podatki/vse-restavracije.json')
-zapisi_csv(podatki_restavracije, ["ime", "kraj", "ocena", "doplacilo", "vegetarijansko", "dostava", "dostop_za_invalide", "odprto_ob_vikendih", "nov_lokal"], 'obdelani-podatki/vse-restavracije.csv')
+zapisi_csv(podatki_restavracije, ["ime", "kraj", "doplacilo", "vegetarijansko", "dostava", "dostop_za_invalide", "odprto_ob_vikendih", "nov_lokal"], 'obdelani-podatki/vse-restavracije.csv')
 
 print(len(podatki_restavracije))
